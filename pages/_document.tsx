408530b9dd2cf631e009880838fb3a8314616b32
Muhammad Aslam <Aslamgondal725@gmail.com>
2026-02-15 01:36:53 +0100

    Add OpenGraph SEO metadata

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,34 @@
-// pages/_document.tsx
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en" suppressHydrationWarning>
-        <Head />
-        <body suppressHydrationWarning>
+      <Html lang="en">
+        <Head>
+
+  {/* OpenGraph */}
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://magondal.com/" />
+  <meta property="og:title" content="Muhammad Aslam | Biofabrication & Organoid Research" />
+  <meta
+    property="og:description"
+    content="Biofabrication of vascularized tissue models, 3D in vitro systems, and bone marrow modeling."
+  />
+  <meta property="og:image" content="https://magondal.com/og.png" />
+
+  {/* Twitter */}
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:url" content="https://magondal.com/" />
+  <meta name="twitter:title" content="Muhammad Aslam | Biofabrication & Organoid Research" />
+  <meta
+    name="twitter:description"
+    content="Biofabrication of vascularized tissue models, 3D in vitro systems, and bone marrow modeling."
+  />
+  <meta name="twitter:image" content="https://magondal.com/og.png" />
+
+</Head>
+
+        <body>
           <Main />
           <NextScript />
         </body>
@@ -14,3 +36,5 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+export default MyDocument;
